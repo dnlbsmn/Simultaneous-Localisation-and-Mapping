7e0c095e2043c8c046f8389deca9853985724478
Hakusan12 <88516865+Hakusan12@users.noreply.github.com>
2022-10-06 15:13:54 +1300

    Add files via upload

--- tan_stuff_jw.py
+++ tan_stuff_jw.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import math
 import numpy as np
-from scipy.ndimage.interpolation import rotate
+#from scipy.ndimage.interpolation import rotate
 
 RAD_PER_PIXEL = 0.001554434
 RAD_OFFSET = 0.497418837
@@ -36,40 +36,61 @@ def interpret_slit(slit_array, x, y, heading):
 
     # Manipulating the left side
     for pixel in range(320):
-        pix_y = float(slit_array[319 - pixel])
-        pix_x = slit_array[319 - pixel] * tan_array[319 - pixel]
+        if (pixel != None):
+            pix_y = float(slit_array[319 - pixel])
+            pix_x = slit_array[319 - pixel] * tan_array[319 - pixel]
 
-        r = math.sqrt(pix_x * pix_x + pix_y * pix_y)
-        phi = math.asin(pix_x/r)
+            r = math.sqrt(pix_x * pix_x + pix_y * pix_y)
+            phi = math.asin(pix_x/r)
 
-        rot_pix_y = int(r*math.cos(- heading + phi))
-        rot_pix_x = int(r*math.sin(- heading + phi))
+            rot_pix_y = int(r*math.cos(- heading + phi))
+            rot_pix_x = int(r*math.sin(- heading + phi))
 
-        if (local_map[rot_pix_y + y][x - rot_pix_x] < 250):
-            local_map[rot_pix_y + y][x - rot_pix_x] += 124
+            if (local_map[rot_pix_y + y][x - rot_pix_x] < 250):
+                local_map[rot_pix_y + y][x - rot_pix_x] += 124
 
     # Manipulating the right side
     for pixel in range(320):
-        pix_y = float(slit_array[639 - pixel])
-        pix_x = slit_array[639 - pixel] * tan_array[pixel]
+        if (pixel != None):
+            pix_y = float(slit_array[639 - pixel])
+            pix_x = slit_array[639 - pixel] * tan_array[pixel]
 
-        r = math.sqrt(pix_x * pix_x + pix_y * pix_y)
-        phi = math.asin(pix_x/r)
-        
-        rot_pix_y = int(r*math.cos(heading + phi))
-        rot_pix_x = int(r*math.sin(heading + phi))
+            r = math.sqrt(pix_x * pix_x + pix_y * pix_y)
+            phi = math.asin(pix_x/r)
+            
+            rot_pix_y = int(r*math.cos(heading + phi))
+            rot_pix_x = int(r*math.sin(heading + phi))
 
-        if (local_map[rot_pix_y + y][rot_pix_x + x + 1] < 250):
-            local_map[rot_pix_y + y][rot_pix_x + x + 1] += 124 #lower 
+            if (local_map[rot_pix_y + y][rot_pix_x + x + 1] < 250):
+                local_map[rot_pix_y + y][rot_pix_x + x + 1] += 124 #lower 
 
 # Display an uncorrected slit as a top down view
 def uncorrected_slit(slit_array):
     for pixel in range(640):
             uncorrected_view[slit_array[pixel]][pixel] = 255
 
+# Clean up noise in generated map
+def clean_image(map):
 
-def clean_image():
-    
+
+    map = cv.blur(map, (8, 8))
+    cv.imshow("display", map)
+    cv.waitKey(0)
+
+    ret, thresh = cv.threshold(map, 30, 255, cv.THRESH_BINARY)
+    map = thresh
+    cv.imshow("display", map)
+    cv.waitKey(0)
+
+    map = cv.erode(map, np.ones((4, 4)), iterations = 1)
+    cv.imshow("display", map)
+    cv.waitKey(0)
+
+    map = cv.dilate(map, np.ones((4, 4)),iterations = 1)
+    cv.imshow("display", map)
+    cv.waitKey(0)
+
+
 
 cv.namedWindow("display", cv.WINDOW_NORMAL)
 cv.resizeWindow("display", 640, 480)
@@ -85,25 +106,22 @@ cv.imshow("display", uncorrected_view)
 cv.waitKey(0)
 
 interpret_slit(slit_array, 601, 601, -0.4)
-uncorrected_slit(slit_array)
-
-cv.imshow("display", local_map)
-cv.waitKey(0)
-
-interpret_slit(slit_array, 601, 601, -0.43)
-
-cv.imshow("display", local_map)
-cv.waitKey(0)
-
+interpret_slit(slit_array, 601, 601, -0.41)
+interpret_slit(slit_array, 601, 601, -0.415)
+interpret_slit(slit_array, 601, 601, -0.42)
 interpret_slit(slit_array, 601, 601, -0.425)
+interpret_slit(slit_array, 602, 602, -0.425)
+interpret_slit(slit_array, 604, 601, -0.425)
+interpret_slit(slit_array, 601, 601, -0.425)
+interpret_slit(slit_array, 601, 601, -0.43)
+interpret_slit(slit_array, 601, 601, -0.457)
+interpret_slit(slit_array, 605, 601, -0.5)
+interpret_slit(slit_array, 599, 608, -0.49)
 
 cv.imshow("display", local_map)
 cv.waitKey(0)
 
-interpret_slit(slit_array, 601, 601, -0.5)
-
-cv.imshow("display", local_map)
-cv.waitKey(0)
+clean_image(local_map)
 
 cv.destroyAllWindows()
 
