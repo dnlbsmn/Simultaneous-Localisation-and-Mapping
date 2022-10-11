3d301accdd12098cabe6aa77bbb97fdabf26e638
dnlbsmn <88516189+dnlbsmn@users.noreply.github.com>
2022-10-11 15:05:26 +1300

    Particle filtering lots of progress

--- particle_lib.py
+++ particle_lib.py
@@ -1,6 +1,16 @@
-import numpy as np
 import cv2 as cv
+import numpy as np
 import math
+
+### ===================================== ###
+# TUNING CONSTANTS
+
+MOTOR_DRIFT_MAX = 0.015
+ANGLE_ERROR_MAX = 0.1
+LINEAR_ERROR_MAX = 0.1
+
+### ===================================== ###
+# FUNCTIONS
 
 # initialises a particle as a dictionary of position, rotation and weight
 def create_particle(x, y, rot, weight, ui_mode = 0):
@@ -68,19 +78,120 @@ def initialise_landmarks(ui_mode = 0):
 			display[lm[1]][lm[0]] = [128, 128, 0]
 
 
-cv.namedWindow("display", cv.WINDOW_NORMAL)
-cv.resizeWindow("display", (1280, 960))
+# Generates a particle given an attempt to move the bot
+def generate_particle(parent, linear, angular):
+    child = {"position": [0, 0], "rotation": 0, "weight": 1}
 
+    a = parent["rotation"]
+    x = parent["position"][0] 
+    y = parent["position"][1]
+
+    # The uncertainty from bot motor imbalance
+    a += np.random.normal(0, MOTOR_DRIFT_MAX, None)
+
+    # The uncertainty from explicit movements
+    a += angular + angular * np.random.normal(0, ANGLE_ERROR_MAX / 3, None)
+    x += linear * math.cos(a) * ( 1 +  np.random.normal(0, LINEAR_ERROR_MAX / 3, None))
+    y += linear * math.sin(a) * ( 1 + np.random.normal(0, LINEAR_ERROR_MAX / 3, None))
+    
+    # Assigning the generated values appropriately
+    child["rotation"] = a
+    child["position"][0] = x
+    child["position"][1] = y
+    
+    return child
+
+
+# Generates an example distribution of a movement
+def example_distributions():
+    p1 = {"position": [31, 0], "rotation": math.pi / 2, "weight": 1}
+
+    for i in range(300):
+        p = generate_particle(p1, 30, 0.5)
+        display_particle(p, 5)
+
+    cv.imshow("display", display)
+    cv.waitKey(0)
+
+
+# Initialises particles to begin particle filtering
+def initialise_particles(particle_count):
+	global display, particles
+
+	cv.namedWindow("display", cv.WINDOW_NORMAL)
+	cv.resizeWindow("display", (640, 480)) 
+	display = np.zeros((64, 64), dtype = np.uint8)
+
+	particles = []
+	parent = {"position": [31, 31], "rotation": 0, "weight": 0}
+
+	for i in range(particle_count):
+		p = generate_particle(parent, 20, 20 * math.pi)
+		display_particle(p, 255)
+		p["weight"] = np.random.rand()
+		particles.append(p)
+
+	cv.imshow("display", display)
+	cv.waitKey(0)
+
+
+# Displays the particles on the matrix
+def display_particle(p, increment):
+    global display
+
+    if (display[int(round(p["position"][1]))][int(round(p["position"][0]))] != 255): 
+        display[int(round(p["position"][1]))][int(round(p["position"][0]))] += increment
+
+
+# Takes the current set of particles and resamples them
+def resample(particle_count, threshold):
+	global display, particles
+
+	total_weight = 0
+	resample = []
+
+	for particle in particles:
+		if (particle["weight"] > threshold):
+			resample.append(particle)
+			total_weight += particle["weight"]
+
+	for particle in resample:
+		particle["weight"] /= total_weight
+
+	particles = resample
+
+	for particle in particles:
+		populate_particle(particle, 5)
+		
+# Populate more particles around a given particle
+def populate_particle(parent, count):
+	global things
+
+	for i in range(count):
+		x = parent["position"][0] + np.random.normal(0, 0.1, None)
+		y = parent["position"][1] + np.random.normal(0, 0.1, None)
+		rot = parent["rotation"] + np.random.normal(0, 0.01, None)
+
+		p = create_particle(x, y, rot, 1)
+		things.append(p)
+
+### ===================================== ###
+# TEST CODE
+
+things = []
+initialise_particles(10)
+
+resample(10, 0.7)
+
+print("###")
 display = np.zeros((64, 64), dtype = np.uint8)
-display = cv.cvtColor(display, cv.COLOR_GRAY2BGR)		
 
-initialise_landmarks(ui_mode = 1)
-
-p1 = create_particle(60, 10, 1, 1, ui_mode = 1)
-
-display[60][9] = [128, 0, 128]
-print(landmarks)
-update_particle(p1, [9, 60], ui_mode = 1)
+for particle in particles:
+	display_particle(particle, 255)
+	print(particle)
 
 cv.imshow("display", display)
 cv.waitKey(0)
+
+for thing in things:
+	print(thing)
